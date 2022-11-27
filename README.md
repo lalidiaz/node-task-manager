f759a48250d05f87cd6e8aefbf01cb7dcf4d4cf8
lauradiaz <lauradiaz1586@gmail.com>
2022-11-27 14:11:26 +0400

    add project logic and FE

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
-# node-task-manager
+#### Task Manager + Mongo DB
+
+In order to run the project, create an account in Mongo DB and setup .env and set MONGO_URI equal to DB connection string.
+To avoid port collisions, in the source code port value is 5000.
+
+
+API [Docs](https://documenter.getpostman.com/view/15377322/2s8Ysuxt42)
+
+**GET** => Get all tasks -> api/v1/tasks
+
+**POST** => Get all tasks -> api/v1/tasks
+
+**GET** => Get task -> api/v1/tasks/:id
+
+**PUT/PATCH** => update task -> api/v1/tasks/:id
+
+**DELETE** => delete task -> api/v1/tasks/:id
+
---
+++ app.js
@@ -0,0 +1,36 @@
+require("./db/connect");
+const express = require("express");
+const app = express();
+const tasks = require("./routes/tasks");
+const connectDB = require("./db/connect");
+require("dotenv").config();
+const notFound = require("./middleware/not-found");
+const errorHandlerMiddleware = require("./middleware/error-handler");
+
+//Middleware
+app.use(express.static("./public"));
+app.use(express.json());
+
+// Routes
+app.get("/", (req, res) => {
+  res.send("Task");
+});
+
+app.use("/api/v1/tasks", tasks);
+app.use(notFound);
+app.use(errorHandlerMiddleware);
+
+const port = process.env.PORT || 3000;
+
+const start = async () => {
+  try {
+    await connectDB(process.env.MONGO_URI);
+    app.listen(port, () => {
+      console.log(`SERVER IS LISTENING ON PORT ${port}`);
+    });
+  } catch (error) {
+    console.log("error", error);
+  }
+};
+
+start();
